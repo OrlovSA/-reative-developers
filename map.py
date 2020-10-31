4ea027d37ce9e16f077430683a49a84900b6be6b
Stivin <68162679+OrlovSA@users.noreply.github.com>
2020-10-31 19:22:53 +0500

    V01

--- map.py
+++ map.py
@@ -2,11 +2,18 @@ import folium
 import geojson
 from folium.plugins import MarkerCluster
 
-def calculations(**kwargs):
-    """Что-то будем вычислять """
-    pass
+ozon = [[43.101010, 131.948338],
+        [43.112434, 131.878115],
+        [43.117154, 131.882482],
+        [43.123695, 131.886836],
+        [43.128426, 131.892511],
+        [43.133207, 131.902996],
+        [43.169214, 131.925219],
+        [43.186311, 131.928825]
+        ]
 
-map = folium.Map(location=[43.164242, 131.905624], zoom_start = 12)
+
+map = folium.Map(location=[43.164242, 131.905624], zoom_start=12)
 marker_cluster = MarkerCluster().add_to(map)
 with open("POI.geojson", encoding='UTF-8') as read_file:
     data = geojson.load(read_file)
@@ -15,24 +22,20 @@ with open("POI.geojson", encoding='UTF-8') as read_file:
         name = data[i].get('properties').get('name')
         pos[0], pos[1] = pos[1], pos[0]
         print(i)
-        folium.CircleMarker(location=pos, popup=name, radius = 9, color="red").add_to(marker_cluster)
+        folium.CircleMarker(location=pos, popup=name, radius=9, color="red").add_to(marker_cluster)
 
-marker_cluster_adm = MarkerCluster().add_to(map)
-with open("Vladivostok_adm.geojson", encoding='UTF-8') as read_file:
-    data = geojson.load(read_file)
-    for num, pos in enumerate(data.get('features')[0].get('geometry').get('coordinates')[0]):
-        pos[0], pos[1] = pos[1], pos[0]
-        print(num)
-        folium.CircleMarker(location=pos, popup='i', radius=9, color="blue").add_to(marker_cluster_adm)
 num = 0
 marker_cluster_buildings = MarkerCluster().add_to(map)
 with open("buildings.geojson", encoding='UTF-8') as read_file:
     data = geojson.load(read_file)
+    # позже разобратся почему не итерирует дата
     for i in range(60902):
-        for pos in data[i].get('geometry').get('coordinates')[0][0]:
-            pos[0], pos[1] = pos[1], pos[0]
-            num += 1
-            print(num)
-            folium.CircleMarker(location=pos, popup=i, radius=1, color="green").add_to(marker_cluster_buildings)
+        if data[i].get('properties').get('building') == 'yes':
+            population = data[i].get('properties').get('population')
+            for pos in data[i].get('geometry').get('coordinates')[0][0]:
+                pos[0], pos[1] = pos[1], pos[0]
+                num += 1
+                print(num)
+                folium.CircleMarker(location=pos, popup=population, radius=9, color="green").add_to(marker_cluster_buildings)
 print('save map')
 map.save("map1.html")
